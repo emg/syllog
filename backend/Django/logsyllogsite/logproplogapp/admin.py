51814f3e8d0c9c67e674a332d4eb42c251887c83
Ulrik Sandborg-Petersen <ulrikp@scripturesys.com>
2021-09-17 20:58:50 +0200

    Deleted, since it was just a temporary measure for comparing with logproplogapp.

--- backend/Django/logsyllogsite/logproplogapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from logproplogapp import models
-
-admin.site.register(models.Answer)
-
-
--- backend/Django/logsyllogsite/logproplogapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LogproplogappConfig(AppConfig):
-    name = 'logproplogapp'
